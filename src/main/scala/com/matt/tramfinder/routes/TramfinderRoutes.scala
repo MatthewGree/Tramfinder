2a2d94247f3773e7f8b8d6dfa714cbad43269a36
mateusz.kmak <mateusz.kmak@ocado.com>
2022-12-12 21:35:05 +0100

    switched route to query parameters

--- src/main/scala/com/matt/tramfinder/routes/TramfinderRoutes.scala
+++ src/main/scala/com/matt/tramfinder/routes/TramfinderRoutes.scala
@@ -1,17 +1,28 @@
 package com.matt.tramfinder.routes
 
 import cats.effect.kernel.Concurrent
-import cats.implicits.{toFlatMapOps, toFunctorOps, toSemigroupKOps}
-import com.matt.tramfinder.routes.model.RouteRequest
+import cats.implicits.toSemigroupKOps
 import com.matt.tramfinder.service.TramfinderService
+import io.circe.syntax.EncoderOps
 import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
-import org.http4s.circe.jsonOf
 import org.http4s.dsl.Http4sDsl
-import org.http4s.{EntityDecoder, HttpRoutes}
+import org.http4s.{HttpRoutes, ParseFailure, QueryParamDecoder}
+
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
+import java.time.Instant
 
 class TramfinderRoutes[F[_] : Concurrent](service: TramfinderService) extends Http4sDsl[F] {
-  private implicit val routesRequestEntityDecoder: EntityDecoder[F, RouteRequest] = jsonOf[F, RouteRequest]
+  private object StartStopQueryParamMatcher extends QueryParamDecoderMatcher[Int]("start")
 
+  private object TargetStopQueryParamMatcher extends QueryParamDecoderMatcher[Int]("target")
+
+  private implicit val instantDecoder: QueryParamDecoder[Instant] = QueryParamDecoder[String]
+    .map(URLDecoder.decode(_, StandardCharsets.UTF_8))
+    .map(_.asJson)
+    .emap(_.as[Instant].fold(error => Left(ParseFailure("", error.message)), Right.apply))
+
+  private object TimeQueryParamMatcher extends QueryParamDecoderMatcher[Instant]("time")
 
   def routes: HttpRoutes[F] =
     getAllNodes <+> findRoute
@@ -23,10 +34,11 @@ class TramfinderRoutes[F[_] : Concurrent](service: TramfinderService) extends Ht
   }
 
   private[routes] def findRoute: HttpRoutes[F] = HttpRoutes.of[F] {
-    case req@GET -> Root / "route" =>
-      for {
-        request <- req.as[RouteRequest]
-        route <- service.findRoute(request.from, request.to, request.time).fold(error => NotFound(error.errorMessage), route => Ok(route))
-      } yield route
+    case GET -> Root / "route"
+      :? StartStopQueryParamMatcher(from)
+      +& TargetStopQueryParamMatcher(to)
+      +& TimeQueryParamMatcher(time) =>
+      service.findRoute(from, to, time)
+        .fold(error => NotFound(error.errorMessage), route => Ok(route))
   }
 }
--- src/test/scala/com/matt/tramfinder/routes/TramfinderRoutesTest.scala
+++ src/test/scala/com/matt/tramfinder/routes/TramfinderRoutesTest.scala
@@ -6,18 +6,19 @@ import cats.implicits.{catsSyntaxEitherId, catsSyntaxOptionId}
 import com.matt.tramfinder.graph.TramStop
 import com.matt.tramfinder.graph.routefinder.{Duration, Route}
 import com.matt.tramfinder.routes.Http4sOps.check
-import com.matt.tramfinder.routes.model.RouteRequest
 import com.matt.tramfinder.service.TramfinderService
 import io.circe.syntax.EncoderOps
 import munit.FunSuite
-import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.circe.jsonDecoder
 import org.http4s.implicits.http4sLiteralsSyntax
-import org.http4s.{Method, Request, Status}
+import org.http4s.{Method, Request, Status, Uri}
 import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.{mock, when}
+import org.mockito.Mockito.{mock, times, verify, when}
 
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 class TramfinderRoutesTest extends FunSuite {
   private val service = mock[TramfinderService](classOf[TramfinderService])
@@ -26,26 +27,27 @@ class TramfinderRoutesTest extends FunSuite {
 
   private val routes = new TramfinderRoutes[IO](service).routes
 
-  override def beforeAll(): Unit = {
-    when(service.getAllStops).thenReturn(stops)
-    when(service.findRoute(any[Int], any[Int], any[Instant])).thenReturn(exampleRoute.asRight)
-  }
-
   test("all nodes route should respond") {
+    when(service.getAllStops).thenReturn(stops)
     val result = routes.run(getNodesRequest).value.unsafeRunSync().get
     check(result, Status.Ok, stops.asJson.some)
+    verify(service, times(1)).getAllStops
   }
 
   test("route node should respond") {
+    when(service.findRoute(any[Int], any[Int], any[Instant])).thenReturn(exampleRoute.asRight)
     val result = routes.run(getRouteRequest).value.unsafeRunSync().get
     check(result, Status.Ok, exampleRoute.asJson.some)
+    verify(service, times(1)).findRoute(any[Int], any[Int], any[Instant])
   }
 
   private def getNodesRequest: Request[IO] =
     Request(method = Method.GET, uri = uri"/stops")
 
-  private def getRouteRequest: Request[IO] =
-    Request[IO](method = Method.GET, uri = uri"/route").withEntity(RouteRequest(1, 2, Instant.now))
+  private def getRouteRequest: Request[IO] = {
+    val instant = URLEncoder.encode(Instant.now.truncatedTo(ChronoUnit.SECONDS).asJson.noSpaces.filter(_ != '\"'), StandardCharsets.UTF_8)
+    Request[IO](method = Method.GET, uri = Uri.fromString(s"/route?start=1&target=2&time=$instant").toTry.get)
+  }
 
 
 }
